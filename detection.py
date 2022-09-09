c9628f1b423c9572fb2b1c42f134d1a7cb10a135
s.kuchuganova <s.kuchuganova@expasoft.tech>
2022-09-09 23:15:43 +0300

    add pushing coords to client

--- detection.py
+++ detection.py
@@ -1,22 +1,37 @@
+import socket
+import time
+
 import cv2
 import numpy as np
+
 from elements.yolo import OBJ_DETECTION
 
+HOST = '0.0.0.0'  # Standard loopback interface address (localhost)
+PORT = 7777  # Port to listen on (non-privileged ports are > 1023)
+
+
 def warp_coords(xy: np.float32, matrix) -> np.float32:
-        return cv2.warpPerspective(xy, matrix, (800, 600), flags=cv2.INTER_LINEAR)
-        return np.matmul(xy, matrix)
+    return cv2.warpPerspective(xy, matrix, (800, 600), flags=cv2.INTER_LINEAR)
+    return np.matmul(xy, matrix)
 
 def get_center_coords(obj) -> list:
         [(xmin, ymin), (xmax, ymax)] = obj['bbox']
         return np.float32([(xmax - xmin) / 2, (ymax - ymin) / 2])
 
 def push_coords(coords: np.float32) -> None:
-        print(coords)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen(1)
+        conn, addr = s.accept()
+        while True:
+            conn.sendall(coords.tobytes())
+            time.sleep(1)
+
 
 VIDEO_SIZE = (1280, 720)
 
 Object_classes = ['cig_butt']
-Object_colors = list(np.random.rand(80,3)*255)
+Object_colors = list(np.random.rand(80, 3) * 255)
 Object_detector = OBJ_DETECTION('weights/best.pt', Object_classes)
 
 # get perspective transformation (calibrate camera)
@@ -26,19 +41,19 @@ warp_matrix = cv2.getPerspectiveTransform(input_pts, output_pts)
 
 cap = cv2.VideoCapture(0)
 if cap.isOpened():
-        while True:
-                ret, frame = cap.read()
-                frame = cv2.resize(frame, VIDEO_SIZE, fx=0, fy=0, interpolation = cv2.INTER_CUBIC)
-                frame = cv2.warpPerspective(frame, warp_matrix, VIDEO_SIZE, flags=cv2.INTER_CUBIC)
-                if ret:
-                        objs = Object_detector.detect(frame)
-                        for obj in objs:
-                                center_coords = get_center_coords(obj)
-                                #warped_coords = warp_coords(center_coords, warp_matrix)
-                                push_coords(center_coords)
-                                #push_coords(warped_coords)
-                        else:
-                                None
-        cap.release()
+    while True:
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, VIDEO_SIZE, fx=0, fy=0, interpolation=cv2.INTER_CUBIC)
+        frame = cv2.warpPerspective(frame, warp_matrix, VIDEO_SIZE, flags=cv2.INTER_CUBIC)
+        if ret:
+            objs = Object_detector.detect(frame)
+            for obj in objs:
+                center_coords = get_center_coords(obj)
+                # warped_coords = warp_coords(center_coords, warp_matrix)
+                push_coords(center_coords)
+                # push_coords(warped_coords)
+            else:
+                None
+    cap.release()
 else:
-        print("Unable to open camera")
+    print("Unable to open camera")
